3a7c0f9407c51320073005822fb6ffed545e3950
ManWuzi <manuel.ziwu23@gmail.com>
2019-07-05 10:20:58 +0100

    Fixed bug in similarity function of Clustering\nPrinting out clusters

--- data_preparation/extract_natural_text.py
+++ data_preparation/extract_natural_text.py
@@ -55,7 +55,7 @@ class Heuristics:
 		if len(line.replace(" ", "")) == 0: return " ".join([z+"_N" for z in line.split(" ")])
 		
 		programming_regexes = ["[\w]+\([\w\W]*\)", "^\s+(for|if|elif|else if)\s?.*\)\s*[:{]?", "<(/)?[\w]+.*>", "}$", "\s+{$", "<<|>>", "^[A-Z][a-z0-9]+(?:[a-z0-9]+|[A-Z]+)+|^[a-z]+[A-Z][a-z0-9]+(?:[a-z0-9]+|[A-Z]+)+", "\"?[\w-]+\"?\s?:\s?(\"|\w+|{|\[(?={?))"]
-		logfile_regexes = ["\d{1,2}:\d{1,2}:\d{1,2}\s", "[a-zA-Z0-9\-]+_\w+[\s:]", "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"]
+		logfile_regexes = ["\d{1,2}:\d{1,2}:\d{1,2}", "[a-zA-Z0-9\-]+_\w+[\s:]", "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", "(\d{4}-\d{2}-\d{2}|\d{2}-\d{2}-\d{4}).*\[.*\]"]
 		for z in programming_regexes+logfile_regexes:
 			k = re.findall(z, line)
 			if (len("".join(k)))/len(line.replace(" ", "")) > self.line_threshold:
@@ -96,7 +96,7 @@ class Clustering:
 
 	#Similarity function
 	def similarity(self, c_1, c_2, l_C):
-		if c_1 == c_2:
+		if c_1[:-2] == c_2[:-2]:
 			return 1
 
 		elif self.class_fn(c_1) == self.class_fn(c_2):
@@ -132,32 +132,106 @@ class Clustering:
 			clusters.remove(sim_cs[1])
 
 			clusters.append(n_c)
+		return clusters
 
 	def run_clustering(self, preprocessed_data):
-		self.bottom_up(preprocessed_data)
+		return self.bottom_up(preprocessed_data)
 
 
 issues = """
-There's some code in List and Tuple to for accessing attributes on the inner objects. This test demonstrates the use case:
+Hi,
+I am running fluentd daemonset on a kubernetes cluster and redirecting logs to splunk. The following given stack trace is not seen at splunk after using the detect exception plugin also.
 
-    def test_list_field_respect_inner_attribute(self):
-        now = dt.datetime.now()
-        obj = DateTimeList([now])
-        field = fields.List(fields.Int(attribute='day'))
-        assert field.serialize('dtimes', obj) == [now.day]
+I have a stack trace like below:
 
-This use case can still be met by using fields.Pluck, though.
+2019-02-06 09:32:51.839 [qtp396180261-19 - /resteasy/exception] INFO  com.intuit.application.SyncControllerImpl - Stack trace of current thread using dumpStack() method
+java.lang.Exception: Stack trace
+	at java.lang.Thread.dumpStack(Thread.java:1336)
+	at com.intuit.application.SyncControllerImpl.createException(SyncControllerImpl.java:38)
+	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.lang.reflect.Method.invoke(Method.java:498)
+	at org.jboss.resteasy.core.MethodInjectorImpl.invoke(MethodInjectorImpl.java:167)
+	at org.jboss.resteasy.core.ResourceMethod.invokeOnTarget(ResourceMethod.java:269)
+	at org.jboss.resteasy.core.ResourceMethod.invoke(ResourceMethod.java:227)
+	at org.jboss.resteasy.core.ResourceMethod.invoke(ResourceMethod.java:216)
+	at org.jboss.resteasy.springmvc.ResteasyHandlerAdapter.createModelAndView(ResteasyHandlerAdapter.java:87)
+	at org.jboss.resteasy.springmvc.ResteasyHandlerAdapter.handle(ResteasyHandlerAdapter.java:74)
+	at org.jboss.resteasy.springmvc.ResteasyHandlerAdapter.handle(ResteasyHandlerAdapter.java:24)
+	at org.jboss.resteasy.springmvc.ResteasyWebHandlerTemplate.handle(ResteasyWebHandlerTemplate.java:39)
+	at org.jboss.resteasy.springmvc.ResteasyHandlerAdapter.handle(ResteasyHandlerAdapter.java:45)
+	at org.springframework.web.servlet.DispatcherServlet.doDispatch(DispatcherServlet.java:963)
+	at org.springframework.web.servlet.DispatcherServlet.doService(DispatcherServlet.java:897)
+	at org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:970)
+	at org.springframework.web.servlet.FrameworkServlet.doGet(FrameworkServlet.java:861)
+	at javax.servlet.http.HttpServlet.service(HttpServlet.java:622)
+	at org.springframework.web.servlet.FrameworkServlet.service(FrameworkServlet.java:846)
+	at javax.servlet.http.HttpServlet.service(HttpServlet.java:729)
+	at org.eclipse.jetty.servlet.ServletHolder.handle(ServletHolder.java:684)
+	at org.eclipse.jetty.servlet.ServletHandler.doHandle(ServletHandler.java:501)
+	at org.eclipse.jetty.server.handler.ScopedHandler.handle(ScopedHandler.java:137)
+	at org.eclipse.jetty.security.SecurityHandler.handle(SecurityHandler.java:557)
+	at org.eclipse.jetty.server.session.SessionHandler.doHandle(SessionHandler.java:231)
+	at org.eclipse.jetty.server.handler.ContextHandler.doHandle(ContextHandler.java:1086)
+	at org.eclipse.jetty.servlet.ServletHandler.doScope(ServletHandler.java:427)
+	at org.eclipse.jetty.server.session.SessionHandler.doScope(SessionHandler.java:193)
+	at org.eclipse.jetty.server.handler.ContextHandler.doScope(ContextHandler.java:1020)
+	at org.eclipse.jetty.server.handler.ScopedHandler.handle(ScopedHandler.java:135)
+	at org.eclipse.jetty.server.handler.HandlerWrapper.handle(HandlerWrapper.java:116)
+	at org.eclipse.jetty.server.Server.handle(Server.java:366)
+	at org.eclipse.jetty.server.AbstractHttpConnection.handleRequest(AbstractHttpConnection.java:494)
+	at org.eclipse.jetty.server.BlockingHttpConnection.handleRequest(BlockingHttpConnection.java:53)
+	at org.eclipse.jetty.server.AbstractHttpConnection.headerComplete(AbstractHttpConnection.java:973)
+	at org.eclipse.jetty.server.AbstractHttpConnection$RequestHandler.headerComplete(AbstractHttpConnection.java:1035)
+	at org.eclipse.jetty.http.HttpParser.parseNext(HttpParser.java:641)
+	at org.eclipse.jetty.http.HttpParser.parseAvailable(HttpParser.java:231)
+	at org.eclipse.jetty.server.BlockingHttpConnection.handle(BlockingHttpConnection.java:72)
+	at org.eclipse.jetty.server.bio.SocketConnector$ConnectorEndPoint.run(SocketConnector.java:264)
+	at org.eclipse.jetty.util.thread.QueuedThreadPool.runJob(QueuedThreadPool.java:608)
+	at org.eclipse.jetty.util.thread.QueuedThreadPool$3.run(QueuedThreadPool.java:543)
+	at java.lang.Thread.run(Thread.java:748)
+displaying Stack trace from StackTraceElement in Java
 
-    def test_list_field_respect_inner_attribute(self):
-        now = dt.datetime.now()
-        obj = DateTimeList([now])
+===================
+This is my fluentd config:
 
-        class DateTimeSchema(Schema):
-            day = fields.Int()
-        field = fields.List(fields.Pluck(DateTimeSchema, 'day'))
-        assert field.serialize('dtimes', obj) == [now.day]
+<source>
+      @type tail
+      format json
+      time_key time
+      time_format %Y-%m-%dT%H:%M:%S.%NZ
+      read_from_head true
 
-This seems like a more consistent API anyway.
+      path /var/log/containers/*.log
+      pos_file /var/log/fluentd/es-containers.log.pos
+      tag raw.kubernetes.*
+    </source>
+
+ # Detect exceptions in the log output and forward them as one log entry.
+    <match raw.kubernetes.**>
+      @id raw.kubernetes
+      @type detect_exceptions
+      remove_tag_prefix raw
+      message message
+      stream stream
+      languages java, python
+      multiline_flush_interval 5
+    </match>
+    # Concatenate multi-line logs
+    <filter kubernetes.**>
+      @id filter_concat
+      @type concat
+      key message
+      multiline_end_regexp /\n$/
+      separator ""
+    </filter>
+
+===============
+I see only first and last line but not the actual stack trace
+java.lang.Exception: Stack trace - this is seen
+displaying Stack trace from StackTraceElement in Java - this is seen
+the in between stack trace is lost
 """
 
 heuristics = Heuristics()
@@ -165,3 +239,4 @@ pp_d = heuristics.run_heuristics(issues)
 
 clustering = Clustering()
 c_d = clustering.run_clustering(pp_d)
+print(c_d)
